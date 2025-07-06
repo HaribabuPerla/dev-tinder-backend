76adaa8490e02c503a7617151de71f6a2c7328ee
haribabuperla04 <93529821+HaribabuPerla1999@users.noreply.github.com>
2025-07-06 17:17:54 +0530

    error handling

--- src/app.js
+++ src/app.js
@@ -6,26 +6,94 @@ require("./config/database")
 const app=express();
 const port=7777; 
 
-app.post("/signup",async(req,res)=>{
-    const userObj={
-        firstName:"Haribabu",
-        lastName:"Perla",
-        emailId:"hari@gmail.com",
-        password:'hari1234',
-   }
+app.use(express.json()); // Middleware to parse JSON bodies
 
-   const user= new User(userObj)
+app.post("/signup",async(req,res)=>{
+  const userObj=req.body
+  const validate=Object.values(userObj)?.every(value=>value?.length>0)
+
+   
 
    try{
+    if(validate){
+        const user=  new User(userObj)
+   console.log("User object created:", user);
      await user.save();
      console.log("user saved successfully",user)
      res.send("User saved successfully");
    }
-   catch{(err)=>{
+    else{
+      res.status(400).send("Some values are not valid or empty");
+    }
+    }
+   catch(err){
        console.error("Error saving user:", err);
        res.status(400).send("Error saving user" + err.message);
-   }}
+   }
 
+})
+app.get("/feed",async(req,res)=>{
+   
+  try{
+    //find is a method to get all the users from mongoose
+        const users= await User.find({}) 
+       if(users?.length > 0){
+          res.send(users)
+       }else{
+         res.status(404).send("No users found");
+       }
+    }catch(err){
+        res.status(500).send("Error fetching users: " + err.message);
+    }
+
+})
+
+app.delete("/user", async(req,res)=>{
+   const userId = req.body.id;
+   try{
+    const deletedUser=await User.findByIdAndDelete(userId)
+    console.log(deletedUser)
+    if(deletedUser){
+        res.send("User deleted successfully");
+    }else{
+        res.status(404).send("User not found");
+    }
+
+   }catch(err){
+         res.status(500).send("Error deleting user: " + err.message);
+   }
+})
+
+app.patch("/user",async(req,res)=>{
+    console.log("Update request received",req.query,req.body);
+
+    const userId = req.query.id;
+    const updateData=req.body
+    console.log("Uupdate:", updateData);
+
+
+    try{
+     if(userId){
+        const allowedUpdateFields=["firstName", "lastName", "password", "age", "photoUrl", "about"];
+        const validUpdtes=Object.keys(updateData).every(key=>allowedUpdateFields.includes(key));
+        if(!validUpdtes){
+            return res.status(400).send("Invalid update fields");
+        }else{
+
+        const updateUser=await User.findByIdAndUpdate(userId,updateData,{returnDocument:"after"})
+        if(updateUser){
+            res.send("User updated successfully");
+        }else{
+            res.status(404).send("User not found");
+        }
+    }  
+    }else{
+        res.status(400).send("User ID is required for update");
+    }
+
+    }catch(err){
+        res.status(500).send("Error updating user: " + err.message);
+    }
 })
 
 connectDB().then(()=>{
@@ -35,6 +103,6 @@ connectDB().then(()=>{
 }) 
 
 }).catch((err)=>{
-    console.error("Database connection failed", err);
+    console.error("Database connection failed", err); 
 }) 
 
--- src/models/user.js
+++ src/models/user.js
@@ -1,27 +1,85 @@
-const mangoose=require("mongoose");
+const mongoose=require("mongoose");  //Importing mongoose
+const validator=require("validator")
 
-const userSchema = new mangoose.Schema({
+if (mongoose.models.User) {
+  delete mongoose.models.User;
+}
+
+//Creating a schema for the user model
+const userSchema = new mongoose.Schema({
     firstName:{
-        type:String
+        type:String,
+        required:true,
+        minLength:3,
+        maxLength:20,
+        trim:true,
     },
     lastName:{
-        type:String
+        type:String,
+        minLength:3,
+        maxLength:20,
+        trim:true,
     },
     emailId:{
-        type:String
+        type:String,
+        required:true,
+        unique:[true,"Email already exists"],
+        lowercase:true,
+        trim:true,
+        validate:(value) => {
+          if(!validator.isEmail(value)){
+            throw new Error("Invalid Email")
+          }
+        }
     },
     password:{
-        type:String
+        type:String,
+        required:true
     },
     age:{
-        type:Number
+        type:Number,
+        min:18,
     },
     gender:{
-        type:String
+        type:String,
+        lowercase:true,
+        validate:(value)=> {
+          const validGenders=["male","female","others"]
+          if(!validGenders.includes(value)){
+            throw new Error("Invalid Gender")
+          }
+        
+    },
+  
+    },
+      photoUrl: {
+    type: String,
+    default: "https://www.w3schools.com/howto/img_avatar.png",
+    validate(value) {
+      if (!validator.isURL(value)) throw new Error("Invalid Photo URL");
     }
+  },
+    about:{
+        type:String,
+        maxLength:100,
+        trim:true,
+        default:"This is a default about me"
+    },
 
 
-})
+},{ timestamps: true })
 
-const User = mangoose.model("User",userSchema);
+userSchema.pre("save", function (next) {
+  if (!this.photoUrl) {
+    this.photoUrl = "https://www.w3schools.com/howto/img_avatar.png";
+  }
+
+  if (!this.about || this.about.trim() === "") {
+    this.about = "This is a default about me";
+  }
+
+  next();
+});
+
+const User = mongoose.model("User",userSchema);
 module.exports=User;
